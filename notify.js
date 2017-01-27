61efe234ee1b2b4ef76ccdcc3472b66a9e2a152e
Reage <rentiansheng@163.com>
2017-01-27 18:59:13 +0800

    fixed close bug

--- notify.js
+++ notify.js
@@ -80,47 +80,26 @@
     window.Notifier.Close = function(type) {
         if(type = 1) {
             tmp = queue.pop();
-            if(tmp.cancel) {
-              tmp.cancel();
-            } else {
-              tmp.close();
-            }
         } else {
           tmp = queue.shift();
-          if(tmp.cancel) {
-            tmp.cancel();
-          } else {
-            tmp.close();
-          }
         }
+        _closeItem(tmp);
     }
 
     window.Notifier.ClosePre = function () {
       tmp = queue.pop();
-      if(tmp.cancel) {
-        tmp.cancel();
-      } else {
-        tmp.close();
-      }
+      _closeItem(tmp);
     }
 
     window.Notifier.CloseLast = function () {
       tmp = queue.shift();
-      if(tmp.cancel) {
-        tmp.cancel();
-      } else {
-        tmp.close();
-      }
+      _closeItem(tmp);
     }
 
     window.Notifier.CloseAll = function() {
         while(queue.length > 0) {
           var tmp =  queue.shift();
-          if(tmp.cancel) {
-            tmp.cancel();
-          } else {
-            tmp.close();
-          }
+          _closeItem(tmp);
         }
     }
 
@@ -128,27 +107,30 @@
     window.Notifier.Notify = function(icon, title, body) {
       if (this.IsGetPermission() == 0) {
 
-        switch(type) {
-            case 2:
-                if(queue.length > 0) {
-                    tmp = queue.pop();
-                    tmp.cancel();
-                }
-                break;
-            case 3:
-                while(queue.length >= c) {
-                    tmp = queue.shift();
-                    tmp.cancel();
-                }
-                break;
-             case 4:
-                setTimeout(function(){popup.cancel();},  t*1000); 
-                break;
-        }
+
         var popup = _createNotificationAndShow(icon, title, body);
         if(undefined == popup) {
           return false;
         }
+
+        switch(type) {
+          case 2:
+            if(queue.length > 0) {
+              tmp = queue.pop();
+              _closeItem(tmp);
+            }
+            break;
+          case 3:
+            while(queue.length >= c) {
+              tmp = queue.shift();
+              _closeItem(tmp);
+            }
+            break;
+          case 4:
+            setTimeout(function(){_closeItem(popup);},  t*1000);
+            break;
+        }
+
         var q = queue;
         popup.onclose = function(){
             var cur = q.indexOf(popup);
@@ -156,6 +138,7 @@
                 q.splice(cur, 1);
             }
         };
+
 
         popup.onclick = function(){};
 
@@ -181,6 +164,17 @@
       } else {
         console.log('error not found notification!')
         return undefined;
+      }
+    }
+
+    function _closeItem(n) {
+      if(undefined == n) {
+        return
+      }
+      if(n.cancel) {
+        n.cancel();
+      } else {
+        n.close();
       }
     }
 
